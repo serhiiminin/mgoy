b25240b3433a820bf4dd11ff3876b487d00b7659
Serhii Minin <minin.serhii@gmail.com>
2019-09-25 10:48:32 +0300

    update rounds of diffs

--- src/util/index.js
+++ src/util/index.js
@@ -1,26 +1,36 @@
-const roundAmount = num => Math.round(num*100)/100;
+const floorAmount = num => Math.floor(num * 100) / 100
+const ceilAmount = num => Math.ceil(num * 100) / 100
 const getTotalAmount = data => data.reduce((acc, { amount }) => acc + amount, 0)
-const getAverage = (totalAmount, count) => count > 0
-  ? Math.round(100* totalAmount / count)/100
-  : 0
-const calculateDiffs = (data, average) =>
-  data.map(item => ({ ...item, diff: roundAmount(item.amount - average) }))
-const sortDataByDiffAsc = data =>
-  data.sort(({ diff: a }, { diff: b }) => a - b)
+const getAverage = (totalAmount, count) =>
+  count > 0 ? floorAmount(totalAmount / count) : 0
+const calculateDiffs = (data, totalAmount) => {
+  const count = data.length
+  const average = getAverage(totalAmount, count)
+  const diffAverage = ceilAmount(totalAmount - count * average)
+
+  return data.map((item, index) => ({
+    ...item,
+    diff: floorAmount(
+      index === 0 && diffAverage > 0
+        ? item.amount - (average + diffAverage)
+        : item.amount - average
+    ),
+  }))
+}
+const sortDataByDiffAsc = data => data.sort(({ diff: a }, { diff: b }) => a - b)
 const checkIsNonZeroDiffPresent = data =>
   Boolean(data.find(({ diff }) => diff !== 0))
 
 const calculatePayments = data => {
   const total = getTotalAmount(data)
-  const count = data.length
-  const average = getAverage(total, count)
-  let diffs = [...sortDataByDiffAsc(calculateDiffs(data, average))]
+  const peopleCount = data.length
+  let diffs = [...sortDataByDiffAsc(calculateDiffs(data, total))]
   let isNonZeroPresent = checkIsNonZeroDiffPresent(diffs)
 
   while (isNonZeroPresent) {
     let updatedDiffs = [...diffs]
     const mustPay = { ...updatedDiffs[0] }
-    const mustGet = { ...updatedDiffs[count - 1] }
+    const mustGet = { ...updatedDiffs[peopleCount - 1] }
     const mustPayDiffAbs = Math.abs(mustPay.diff)
 
     if (mustPayDiffAbs >= mustGet.diff) {
@@ -28,7 +38,7 @@ const calculatePayments = data => {
         name: mustGet.name,
         amount: mustGet.diff,
       })
-      mustPay.diff = mustPay.diff + mustGet.diff
+      mustPay.diff = floorAmount(mustPay.diff + mustGet.diff)
       mustGet.diff = 0
     }
 
@@ -38,13 +48,14 @@ const calculatePayments = data => {
         amount: mustPayDiffAbs,
       })
       mustPay.diff = 0
-      mustGet.diff = mustGet.diff - mustPayDiffAbs
+      mustGet.diff = ceilAmount(mustGet.diff - mustPayDiffAbs)
     }
 
-    updatedDiffs = [mustPay, ...updatedDiffs.slice(1, count - 1), mustGet]
+    updatedDiffs = [mustPay, ...updatedDiffs.slice(1, peopleCount - 1), mustGet]
 
     diffs = sortDataByDiffAsc(updatedDiffs)
     isNonZeroPresent = checkIsNonZeroDiffPresent(diffs)
+    console.log(JSON.stringify(diffs))
   }
 
   return diffs
